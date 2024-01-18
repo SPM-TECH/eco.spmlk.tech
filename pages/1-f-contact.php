bf84caf548175966371c2aad696484f1f36db8f5
FathimaSamila99 <samila9926@gmail.com>
2024-01-18 12:26:09 +0530

    edit-contact us

--- pages/1-f-contact.php
+++ pages/1-f-contact.php
@@ -44,26 +44,26 @@
         </div>
 
         <div class="col-lg-6">
-          <form action="https://docs.google.com/forms/d/1BBSbpRCcSVWHfHpw-6EtiFt4yZg3uaxrMcruNC0sAnI/prefill" method="post" role="form" class="php-email-form" onsubmit="return window.submitGoogleForm(this);">
+          <form action="mail.php" method="post" role="form" class="php-email-form" >
             <div class="form-row">
               <div class="col-md-6 form-group">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" name="entry.1713471508" required/>
+                <input type="text" name="username" class="form-control" id="name" placeholder="Your Name"  required/>
                 <div class="validate"></div>
               </div>
               <div class="col-md-6 form-group">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" name="emailAddress" required/>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email"  required/>
                 <div class="validate"></div>
               </div>
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" name="entry.1315428394" required/>
+              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"  required/>
               <div class="validate"></div>
             </div>
             <div class="form-group">
-              <textarea class="form-control" name="message" rows="5" name="entry.1465059479" required></textarea>
+              <textarea class="form-control" name="message" rows="5"  required></textarea>
               <div class="validate"></div>
             </div>
-            <div class="text-center"><button type="submit">Send Message</button></div>
+            <div class="text-center"><button type="submit" name="submit">Send Message</button></div>
           </form>
         </div>
 
